fd250fec7b0a607e1f76bc8b2ce3ff227d477005
Linus Skucas <git@linus.sh>
2023-01-26 16:36:23 -0800

    Drivetrain and Joystick (#31)
    Co-authored-by: Andrew Day <flarethesaberdragon@gmail.com>
    Co-authored-by: Andrew Day <103972011+FlareTheSaberDragon@users.noreply.github.com>
    Co-authored-by: SBsteveyHops <salvobonsma@gmail.com>
    Co-authored-by: github-actions <>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,20 @@
 package frc.robot;
 
 public final class Constants {
-  
+  // Joystick
+  public static final double DEADBAND = 0.06;
+  public static final double EXPONENT = 0.1; // between 0 and 1
+
+  // Drivetrain
+  public static final double DRIVE_SPEED_MULT = 1.0; // should be between 0 and 1
+  public static final double DRIVE_SPEED_AUTO = 0.1 / DRIVE_SPEED_MULT;
+  public static final int DRIVE_CURRENT_LIMIT = 40;
+  public static final double MOVEMENT_SPEED = 1; // meters per second
+  public static final double TURN_SPEED = 1; // radians per second
+
+  // Ports
+  public static final int WHEEL_PORT_FRONT_LEFT = 1;
+  public static final int WHEEL_PORT_REAR_LEFT = 3;
+  public static final int WHEEL_PORT_FRONT_RIGHT = 2;
+  public static final int WHEEL_PORT_REAR_RIGHT = 4;
 }
---
+++ src/main/java/frc/robot/subsystems/MecanumDrive.java
@@ -0,0 +1,98 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
+import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class MecanumDrive extends SubsystemBase {
+  private boolean inverted;
+  private CANSparkMax frontLeftMotor;
+  private CANSparkMax backLeftMotor;
+  private CANSparkMax frontRightMotor;
+  private CANSparkMax backRightMotor;
+
+  private final MecanumDriveKinematics kinematics;
+
+  public MecanumDrive() {
+    frontLeftMotor = new CANSparkMax(Constants.WHEEL_PORT_FRONT_LEFT, MotorType.kBrushless);
+    frontRightMotor = new CANSparkMax(Constants.WHEEL_PORT_FRONT_RIGHT, MotorType.kBrushless);
+    backLeftMotor = new CANSparkMax(Constants.WHEEL_PORT_REAR_LEFT, MotorType.kBrushless);
+    backRightMotor = new CANSparkMax(Constants.WHEEL_PORT_REAR_LEFT, MotorType.kBrushless);
+
+    frontLeftMotor.setInverted(true);
+    frontRightMotor.setInverted(false);
+    backLeftMotor.setInverted(true);
+    backRightMotor.setInverted(false);
+    inverted = false;
+
+    frontLeftMotor.setSmartCurrentLimit(Constants.DRIVE_CURRENT_LIMIT);
+    frontRightMotor.setSmartCurrentLimit(Constants.DRIVE_CURRENT_LIMIT);
+    backLeftMotor.setSmartCurrentLimit(Constants.DRIVE_CURRENT_LIMIT);
+    backRightMotor.setSmartCurrentLimit(Constants.DRIVE_CURRENT_LIMIT);
+
+    frontLeftMotor.setIdleMode(IdleMode.kBrake);
+    frontRightMotor.setIdleMode(IdleMode.kBrake);
+    backLeftMotor.setIdleMode(IdleMode.kBrake);
+    backRightMotor.setIdleMode(IdleMode.kBrake);
+
+    // meter per second
+    kinematics =
+        new MecanumDriveKinematics(
+            new Translation2d(0.28575, 0.2267),
+            new Translation2d(0.28575, -0.2267),
+            new Translation2d(-0.28575, 0.2267),
+            new Translation2d(-0.28575, -0.2267));
+  }
+
+  public void invertDrive() {
+    inverted = !inverted;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    super.periodic();
+
+    SmartDashboard.putBoolean("isInverted", inverted);
+  }
+
+  public void updateSpeed(double strafe, double drive, double turn, boolean useInverted) {
+    double xSpeed = drive * Constants.MOVEMENT_SPEED;
+    double ySpeed = strafe * Constants.MOVEMENT_SPEED;
+    if (useInverted && inverted) {
+      xSpeed = -xSpeed;
+      ySpeed = -ySpeed;
+    }
+
+    ChassisSpeeds chassisSpeeds = new ChassisSpeeds(xSpeed, ySpeed, turn * -Constants.TURN_SPEED);
+    MecanumDriveWheelSpeeds wheelSpeeds = kinematics.toWheelSpeeds(chassisSpeeds);
+
+    this.frontLeftMotor.set(
+        wheelSpeeds.frontLeftMetersPerSecond
+            * Constants.DRIVE_SPEED_MULT
+            / Constants.MOVEMENT_SPEED);
+    this.frontRightMotor.set(
+        wheelSpeeds.frontRightMetersPerSecond
+            * Constants.DRIVE_SPEED_MULT
+            / Constants.MOVEMENT_SPEED);
+    this.backLeftMotor.set(
+        wheelSpeeds.rearLeftMetersPerSecond
+            * Constants.DRIVE_SPEED_MULT
+            / Constants.MOVEMENT_SPEED);
+    this.backRightMotor.set(
+        wheelSpeeds.rearRightMetersPerSecond
+            * Constants.DRIVE_SPEED_MULT
+            / Constants.MOVEMENT_SPEED);
+  }
+
+  public void stop() {
+    updateSpeed(0, 0, 0, false);
+  }
+}
