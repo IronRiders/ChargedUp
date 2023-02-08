5954356111afb069ff2ba7eff14cdbecf65edef2
Andrew Day <103972011+FlareTheSaberDragon@users.noreply.github.com>
2023-02-08 15:33:30 -0800

    Added differential drive capability (#55)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,6 @@ package frc.robot;
 
 import frc.robot.subsystems.ManipulatorSubsystem;
 import frc.robot.subsystems.GrabObject;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
@@ -23,6 +22,9 @@ import frc.robot.subsystems.*;
 public class RobotContainer {
   // Subsystems
   public final ManipulatorSubsystem manipulator = new ManipulatorSubsystem();
+
+  // one of the two following lines of code must be commented out at all times
+  // public final DifferentialDrive drive = new DifferentialDrive();
   public final MecanumDrive drive = new MecanumDrive();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
