a6600eb737dde9661f3f1505ad77913bbb59833f
Andrew Day <flarethesaberdragon@gmail.com>
2023-02-08 14:42:08 -0800

    Updated dif drive to 4 wheels

--- src/main/java/frc/robot/subsystems/DiferentialDrive.java
+++ src/main/java/frc/robot/subsystems/DiferentialDrive.java
@@ -16,10 +16,8 @@ import frc.robot.Constants;
 public class DiferentialDrive extends SubsystemBase {
   private boolean inverted;
   private CANSparkMax frontLeftMotor;
-  private CANSparkMax middleLeftMotor;
   private CANSparkMax backLeftMotor;
   private CANSparkMax frontRightMotor;
-  private CANSparkMax middleRightMotor;
   private CANSparkMax backRightMotor;
 
   private final DifferentialDriveKinematics kinematics;
@@ -27,30 +25,22 @@ public class DiferentialDrive extends SubsystemBase {
   public DiferentialDrive() {
     frontLeftMotor = new CANSparkMax(Constants.WHEEL_PORT_FRONT_LEFT, MotorType.kBrushless);
     frontRightMotor = new CANSparkMax(Constants.WHEEL_PORT_FRONT_RIGHT, MotorType.kBrushless);
-    middleLeftMotor = new CANSparkMax(Constants.WHEEL_PORT_FRONT_LEFT, MotorType.kBrushless);
-    middleRightMotor = new CANSparkMax(Constants.WHEEL_PORT_FRONT_RIGHT, MotorType.kBrushless);
     backLeftMotor = new CANSparkMax(Constants.WHEEL_PORT_REAR_LEFT, MotorType.kBrushless);
     backRightMotor = new CANSparkMax(Constants.WHEEL_PORT_REAR_LEFT, MotorType.kBrushless);
 
     frontLeftMotor.setInverted(true);
     frontRightMotor.setInverted(false);
-    middleLeftMotor.setInverted(true);
-    middleRightMotor.setInverted(false);
     backLeftMotor.setInverted(true);
     backRightMotor.setInverted(false);
     inverted = false;
 
     frontLeftMotor.setSmartCurrentLimit(Constants.DRIVE_CURRENT_LIMIT);
     frontRightMotor.setSmartCurrentLimit(Constants.DRIVE_CURRENT_LIMIT);
-    middleLeftMotor.setSmartCurrentLimit(Constants.DRIVE_CURRENT_LIMIT);
-    middleRightMotor.setSmartCurrentLimit(Constants.DRIVE_CURRENT_LIMIT);
     backLeftMotor.setSmartCurrentLimit(Constants.DRIVE_CURRENT_LIMIT);
     backRightMotor.setSmartCurrentLimit(Constants.DRIVE_CURRENT_LIMIT);
 
     frontLeftMotor.setIdleMode(IdleMode.kBrake);
     frontRightMotor.setIdleMode(IdleMode.kBrake);
-    middleLeftMotor.setIdleMode(IdleMode.kBrake);
-    middleRightMotor.setIdleMode(IdleMode.kBrake);
     backLeftMotor.setIdleMode(IdleMode.kBrake);
     backRightMotor.setIdleMode(IdleMode.kBrake);
 
@@ -86,9 +76,9 @@ public class DiferentialDrive extends SubsystemBase {
 // Right velocity
     double rightVelocity = wheelSpeeds.rightMetersPerSecond;
 
-    MotorControllerGroup leftMotors = new MotorControllerGroup(backLeftMotor, middleLeftMotor, frontLeftMotor);
+    MotorControllerGroup leftMotors = new MotorControllerGroup(backLeftMotor, frontLeftMotor);
 
-    MotorControllerGroup rightMotors = new MotorControllerGroup(backRightMotor, middleRightMotor, frontRightMotor);
+    MotorControllerGroup rightMotors = new MotorControllerGroup(backRightMotor, frontRightMotor);
 
     leftMotors.set(leftVelocity);
     rightMotors.set(rightVelocity);
