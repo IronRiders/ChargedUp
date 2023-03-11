044ebc71a27c30d4471dcdec387051a1795da992
Linus Skucas <git@linus.sh>
2023-03-11 08:01:33 +0000

    More Fixes

--- src/main/java/frc/robot/commands/BurnFlashCommand.java
+++ src/main/java/frc/robot/commands/BurnFlashCommand.java
@@ -2,6 +2,9 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ManipulatorSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ExtendingArmSubsystem;
+import frc.robot.subsystems.RaiseLowerArmSubsystem;
 
 public class BurnFlashCommand extends CommandBase {
   private final ManipulatorSubsystem manipulatorSubsystem;
