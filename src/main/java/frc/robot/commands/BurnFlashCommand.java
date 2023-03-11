37dbfc48f16c3f2ca7fdabaaa31c5ae45703e27d
Linus Skucas <git@linus.sh>
2023-03-11 07:47:17 +0000

    Add BurnFlash Command

---
+++ src/main/java/frc/robot/commands/BurnFlashCommand.java
@@ -0,0 +1,32 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ManipulatorSubsystem;
+
+public class BurnFlashCommand extends CommandBase {
+  private final ManipulatorSubsystem manipulatorSubsystem;
+  private final DriveSubsystem driveSubsystem;
+  private final ExtendingArmSubsystem extendingArmSubsystem;
+  private final RaiseLowerArmSubsystem raiseLowerArmSubsystem;
+
+  public GrabManipulatorCommand(DriveSubsystem driveSubsystem, ExtendingArmSubsystem extendingArmSubsystem, RaiseLowerArmSubsystem raiseLowerArmSubsystem, ManipulatorSubsystem manipulatorSubsystem) {
+	this.manipulatorSubsystem = manipulatorSubsystem;
+	this.driveSubsystem = driveSubsystem
+	this.extendingArmSubsystem = extendingArmSubsystem
+	this.raiseLowerArmSubsystem = raiseLowerArmSubsystem
+	addRequirements(driveSubsyste, manipulatorSubsystem, extendingArmSubsystem, raiseLowerArmSubsystem, manipulatorSubsystem);
+  }
+
+  @Override
+  public void initialize() {
+	manipulatorSubsystem.burnFlash();
+	driveSubsystem.burnFlash();
+	extendingArmSubsystem.burnFlash();
+	raiseLowerArmSubsystem.burnFlash();
+  }
+
+  @Override
+  public boolean isFinished() {
+	return true;
+  }
+}
